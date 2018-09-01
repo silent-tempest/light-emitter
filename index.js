9784ff82e69670d0c4b5830f1dd3ed1c2d12553b
silent-tempest <brussian768@gmail.com>
2018-09-01 17:52:46 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,139 @@
+'use strict';
+
+/**
+ * A lightweight implementation of Node's EventEmitter.
+ * @constructor LightEmitter
+ * @example
+ * var LightEmitter = require( 'light-emitter' );
+ */
+function LightEmitter () {}
+
+LightEmitter.prototype = {
+  /**
+   * @method LightEmitter#emit
+   * @param {string} type
+   * @param {...any} [data]
+   * @chainable
+   */
+  emit: function emit ( type ) {
+    var list = _getList( this, type );
+    var data, i, l;
+
+    if ( ! list ) {
+      return this;
+    }
+
+    if ( arguments.length > 1 ) {
+      data = [].slice.call( arguments, 1 );
+    }
+
+    for ( i = 0, l = list.length; i < l; ++i ) {
+      if ( ! list[ i ].active ) {
+        continue;
+      }
+
+      if ( list[ i ].once ) {
+        list[ i ].active = false;
+      }
+
+      if ( data ) {
+        list[ i ].listener.apply( this, data );
+      } else {
+        list[ i ].listener.call( this );
+      }
+    }
+
+    return this;
+  },
+
+  /**
+   * @method LightEmitter#off
+   * @param {string}   [type]
+   * @param {function} [listener]
+   * @chainable
+   */
+  off: function off ( type, listener ) {
+    var list, i;
+
+    if ( ! type ) {
+      this._events = null;
+    } else if ( ( list = _getList( this, type ) ) ) {
+      if ( listener ) {
+        for ( i = list.length - 1; i >= 0; --i ) {
+          if ( list[ i ].listener === listener && list[ i ].active ) {
+            list[ i ].active = false;
+          }
+        }
+      } else {
+        list.length = 0;
+      }
+    }
+
+    return this;
+  },
+
+  /**
+   * @method LightEmitter#on
+   * @param {string}   type
+   * @param {function} listener
+   * @chainable
+   */
+  on: function on ( type, listener ) {
+    _on( this, type, listener );
+    return this;
+  },
+
+  /**
+   * @method LightEmitter#once
+   * @param {string}   type
+   * @param {function} listener
+   * @chainable
+   */
+  once: function once ( type, listener ) {
+    _on( this, type, listener, true );
+    return this;
+  },
+
+  constructor: LightEmitter
+};
+
+/**
+ * @private
+ * @method _on
+ * @param  {LightEmitter} self
+ * @param  {string}       type
+ * @param  {function}     listener
+ * @param  {boolean}      once
+ * @return {void}
+ */
+function _on ( self, type, listener, once ) {
+  var entity = {
+    listener: listener,
+    active:   true,
+    type:     type,
+    once:     once
+  };
+
+  if ( ! self._events ) {
+    self._events = Object.create( null );
+  }
+
+  if ( ! self._events[ type ] ) {
+    self._events[ type ] = [];
+  }
+
+  self._events[ type ].push( entity );
+}
+
+/**
+ * @private
+ * @method _getList
+ * @param  {LightEmitter}   self
+ * @param  {string}         type
+ * @return {array<object>?}
+ */
+function _getList ( self, type ) {
+  return self._events && self._events[ type ];
+}
+
+module.exports = LightEmitter;
